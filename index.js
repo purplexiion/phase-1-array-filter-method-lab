8a2bc1bbb5b749072a75df6e825bbe3fc0f5d3d6
Wamuyu <michellewachira25@gmail.com>
2022-06-13 11:38:18 +0300

    commit tree 13ba13d6dd0fa9d71467fd359a018ba2d010406e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"]
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function(driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function(driver) {
+        return driver.charAt(0) === attribute.charAt(0)
+    })
+} //recall :charAt(0) --- PICK OUT A CHARACTER IN A STRING
+
+
+function matchName(drivers, argument) {
+    return drivers.filter(function(driver) {
+        return driver.name === argument
+
+
+    })
+}
